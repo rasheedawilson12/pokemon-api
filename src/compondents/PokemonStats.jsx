9cea946a5ed20cd2799e00f2c85fa34071450726
rasheedawilson12 <rasheedawilson12@gmail.com>
2023-08-02 10:04:09 -0500

    Attacks and base stats added

--- src/compondents/PokemonStats.jsx
+++ src/compondents/PokemonStats.jsx
@@ -1,19 +1,37 @@
 import React from "react";
 
 function PokemonStats(props) {
-  // return !props.data ? (
-  //   ""
-  // ) : (
-  //   <div className="PokemonStats">
-  //     <h1>{props.data.name}</h1>
-  //     <img
-  //       src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${props.data.id}.svg`}
-  //       alt={props.data.name}
-  //     />
-  //     <div className="attacks"></div>
-  //     <div className="base-stats"></div>
-  //   </div>
-  // );
+  return !props.data ? (
+    ""
+  ) : (
+    <div className="PokemonStats">
+      <h1>{props.data.name}</h1>
+      <img
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${props.data.id}.svg`}
+        alt={props.data.name}
+      />
+      <div className="attacks">
+        {props.data.abilities.map((attack) => {
+          return (
+            <div>
+              <h2>{attack.ability.name}</h2>
+            </div>
+          );
+        })}
+      </div>
+      <div className="base-stats">
+        {props.data.stats.map((stats) => {
+          return (
+            <div>
+              <h3>
+                {stats.stat.name}: {stats.base_stat}
+              </h3>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default PokemonStats;
