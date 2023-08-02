9689c1d0991b1c9bab5dad4b1d75448ff16d94f8
rasheedawilson12 <rasheedawilson12@gmail.com>
2023-08-02 11:21:04 -0500

    Additional info added to PokemonStats component

--- src/compondents/Nav.jsx
+++ src/compondents/Nav.jsx
@@ -6,7 +6,26 @@ function Nav() {
       <h1>Pokemon API Pokedex</h1>
       <select className="selectType">
         <option value="">Select a Type</option>
-        <option></option>
+        <option>Normal</option>
+        <option>Fire</option>
+        <option>Water</option>
+        <option>Grass</option>
+        <option>Electric</option>
+        <option>Ice</option>
+        <option>Fighting</option>
+        <option>Poison</option>
+        <option>Ground</option>
+        <option>Flying</option>
+        <option>Psychic</option>
+        <option>Bug</option>
+        <option>Rock</option>
+        <option>Ghost</option>
+        <option>Dark</option>
+        <option>Dragon</option>
+        <option>Steel</option>
+        <option>Fairy</option>
+        <option>Shadow</option>
+        <option>Unknown</option>
       </select>
     </div>
   );
--- src/compondents/PokemonStats.jsx
+++ src/compondents/PokemonStats.jsx
@@ -20,16 +20,30 @@ function PokemonStats(props) {
           );
         })}
       </div>
-      <div className="base-stats">
-        {props.data.stats.map((stats) => {
-          return (
-            <div>
-              <h3>
-                {stats.stat.name}: {stats.base_stat}
-              </h3>
-            </div>
-          );
-        })}
+      <div className="base-statsContainer">
+        <div className="baseInfo">
+          <h3>Height: {props.data.height}</h3>
+          <h3>Weight: {props.data.weight}</h3>
+          {props.data.types.map((type) => {
+            return (
+              <div>
+                <h3>Type:</h3>
+                <span>{type.type.name}</span>
+              </div>
+            );
+          })}
+        </div>
+        <div className="base-stats">
+          {props.data.stats.map((stats) => {
+            return (
+              <div>
+                <h3>
+                  {stats.stat.name}: {stats.base_stat}
+                </h3>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
