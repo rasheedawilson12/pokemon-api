3dae8890b3396d36a47c96c81b05d20c9da3e920
rasheedawilson12 <rasheedawilson12@gmail.com>
2023-08-04 16:39:12 -0500

    navbar images added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
 import Nav from "./compondents/Nav";
 import Main from "./pages/Main";
+
 // import Pagination from Pagination;
 
 function App() {
   return (
     <div>
       <Nav />
-      <Routes>
-        <Route path="/" element={<Main />} />
-      </Routes>
+      <Main />
     </div>
   );
 }
--- src/compondents/Nav.jsx
+++ src/compondents/Nav.jsx
@@ -4,29 +4,85 @@ function Nav() {
   return (
     <div className="navbar">
       <h1>Pokemon API Pokedex</h1>
-      <select className="selectType">
-        <option value="">Select a Type</option>
-        <option>Normal</option>
-        <option>Fire</option>
-        <option>Water</option>
-        <option>Grass</option>
-        <option>Electric</option>
-        <option>Ice</option>
-        <option>Fighting</option>
-        <option>Poison</option>
-        <option>Ground</option>
-        <option>Flying</option>
-        <option>Psychic</option>
-        <option>Bug</option>
-        <option>Rock</option>
-        <option>Ghost</option>
-        <option>Dark</option>
-        <option>Dragon</option>
-        <option>Steel</option>
-        <option>Fairy</option>
-        <option>Shadow</option>
-        <option>Unknown</option>
-      </select>
+      <div className="typeImgs">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Pok%C3%A9mon_Normal_Type_Icon.svg/512px-Pok%C3%A9mon_Normal_Type_Icon.svg.png?20230404224439"
+          alt="normalType"
+        />
+        <img
+          src="https://image.pngaaa.com/886/6175886-middle.png"
+          alt="fireType"
+        />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Pok%C3%A9mon_Water_Type_Icon.svg/512px-Pok%C3%A9mon_Water_Type_Icon.svg.png?20200511081703"
+          alt="waterType"
+        />
+        <img
+          src="https://image.pngaaa.com/710/4835710-middle.png"
+          alt="grassType"
+        />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Pok%C3%A9mon_Electric_Type_Icon.svg/512px-Pok%C3%A9mon_Electric_Type_Icon.svg.png?20200511085231"
+          alt="electricType"
+        />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Pok%C3%A9mon_Ice_Type_Icon.svg/512px-Pok%C3%A9mon_Ice_Type_Icon.svg.png?20200511090905"
+          alt="iceType"
+        />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Pok%C3%A9mon_Fighting_Type_Icon.svg/512px-Pok%C3%A9mon_Fighting_Type_Icon.svg.png?20200511092020"
+          alt="fightingType"
+        />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Pok%C3%A9mon_Poison_Type_Icon.svg/2048px-Pok%C3%A9mon_Poison_Type_Icon.svg.png"
+          alt="poisonType"
+        />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Pok%C3%A9mon_Ground_Type_Icon.svg/512px-Pok%C3%A9mon_Ground_Type_Icon.svg.png?20200511091001"
+          alt="groundType"
+        />
+        <img
+          src="https://pixelmonmod.com/w/images/0/0d/FlyingType.png"
+          alt="flyingType"
+        />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Pok%C3%A9mon_Psychic_Type_Icon.svg/512px-Pok%C3%A9mon_Psychic_Type_Icon.svg.png?20200511092630"
+          alt="psychicType"
+        />
+        <img src="https://bugalert.org/images/bug.png" alt="bugType" />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Pok%C3%A9mon_Rock_Type_Icon.svg/512px-Pok%C3%A9mon_Rock_Type_Icon.svg.png?20200511083843"
+          alt="rockType"
+        />
+        <img
+          src="https://image.pngaaa.com/884/6175884-middle.png"
+          alt="ghostType"
+        />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Pok%C3%A9mon_Dark_Type_Icon.svg/512px-Pok%C3%A9mon_Dark_Type_Icon.svg.png?20200511085509"
+          alt="darkType"
+        />
+        <img
+          src="https://image.pngaaa.com/154/5056154-middle.png"
+          alt="dragonType"
+        />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Pok%C3%A9mon_Steel_Type_Icon.svg/512px-Pok%C3%A9mon_Steel_Type_Icon.svg.png?20200511081737"
+          alt="steelType"
+        />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Pok%C3%A9mon_Fairy_Type_Icon.svg/512px-Pok%C3%A9mon_Fairy_Type_Icon.svg.png?20200511094221"
+          alt="fairyType"
+        />
+        {/* <img
+          src="https://w7.pngwing.com/pngs/85/390/png-transparent-pokemon-type-element-design-symbol-sign-icon-normal-basic-star-thumbnail.png"
+          alt="normalType"
+        />
+        <img
+          src="https://w7.pngwing.com/pngs/85/390/png-transparent-pokemon-type-element-design-symbol-sign-icon-normal-basic-star-thumbnail.png"
+          alt="normalType"
+        /> */}
+      </div>
     </div>
   );
 }
